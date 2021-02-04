6c0f99a3a7d17b6599450bf5ec121255f6d9243f
AndreasElia <andreaselia@live.co.uk>
2021-02-04 16:19:57 +0000

    styleci fix

--- examples/api.php
+++ examples/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
