e4a28bec8b72760beb2442e9ef3bf85cfe80b028
tomirons <tom.irons@hotmail.com>
2021-02-14 17:43:37 -0500

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,11 +13,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function defineRoutes($router)
     {
-        $router->middleware('api')->group(function ($router) {
-            $router->get('index', [ExampleController::class, 'index']);
-            $router->get('show', [ExampleController::class, 'show']);
-            $router->post('store', [ExampleController::class, 'store']);
-            $router->delete('delete', [ExampleController::class, 'delete']);
+        $router->middleware('api')->prefix('example')->name('example.')->group(function ($router) {
+            $router->get('index', [ExampleController::class, 'index'])->name('index');
+            $router->get('show', [ExampleController::class, 'show'])->name('show');
+            $router->post('store', [ExampleController::class, 'store'])->name('store');
+            $router->delete('delete', [ExampleController::class, 'delete'])->name('delete');
         });
     }
 }
