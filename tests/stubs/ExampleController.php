6abc8e22c2a4753ef7e8754a378ae311a9404db3
tomirons <tom.irons@hotmail.com>
2021-02-14 17:41:11 -0500

    wip

---
+++ tests/stubs/ExampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AndreasElia\Tests\Stubs;
+
+use App\Http\Controllers\Controller;
+
+class ExampleController extends Controller
+{
+//    public function index() {}
+//
+//    public function show() {}
+//
+//    public function store() {}
+//
+//    public function delete() {}
+}
