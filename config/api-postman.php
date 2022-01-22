a71ad85d8c3711b990d718e474646866b1505799
Robert Marney <robert.marney@vistaprojects.com>
2022-01-22 13:23:05 -0700

    Move Config

--- config/api-postman.php
+++ config/api-postman.php
@@ -37,17 +37,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Use Method Doc Tags to generate Request Description
-    |--------------------------------------------------------------------------
-    |
-    | Should we take the non @ text of the method docs to describe the methods?
-    |
-    */
-
-    'extract_description_from_controller' => false,
-
-    /*
-    |--------------------------------------------------------------------------
     | Auth Middleware
     |--------------------------------------------------------------------------
     |
@@ -102,6 +91,18 @@ return [
 
     'print_rules' => true, // @requires: 'enable_formdata' ===  true
     'rules_to_human_readable' => true, // @requires: 'parse_rules' ===  true
+
+    /*
+    |--------------------------------------------------------------------------
+    | Use Method Doc Tags to generate Request Description
+    |--------------------------------------------------------------------------
+    |
+    | Should we take the non @ text of the method docs to describe the requests
+    | within the generated collection?
+    |
+    */
+
+        'extract_description_from_controller' => false,
 
     /*
     |--------------------------------------------------------------------------
