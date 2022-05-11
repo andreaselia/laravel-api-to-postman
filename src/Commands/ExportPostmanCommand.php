0246d3df148db8abce41a0d2bdd477b0c9bc4d00
Tom Irons <tom.irons@hotmail.com>
2022-05-11 16:02:31 -0400

    more cleanup

--- src/Commands/ExportPostmanCommand.php
+++ src/Commands/ExportPostmanCommand.php
@@ -2,74 +2,39 @@
 
 namespace AndreasElia\PostmanGenerator\Commands;
 
-use Closure;
-use ReflectionClass;
-use ReflectionFunction;
 use Illuminate\Support\Str;
 use Illuminate\Routing\Router;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Contracts\Validation\Rule;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Contracts\Config\Repository;
-use Illuminate\Foundation\Http\FormRequest;
-use AndreasElia\PostmanGenerator\PostmanMapper;
-use Illuminate\Validation\ValidationRuleParser;
+use AndreasElia\PostmanGenerator\PostmanExporter;
 
 class ExportPostmanCommand extends Command
 {
-    /** @var string */
     protected $signature = 'export:postman
                         {--bearer= : The bearer token to use on your endpoints}
                         {--basic= : The basic auth to use on your endpoints}';
 
-    /** @var string */
     protected $description = 'Automatically generate a Postman collection for your API routes';
 
-    /** @var \Illuminate\Routing\Router */
-    protected $router;
+    protected Router $router;
 
-    /** @var array */
-    protected $structure;
+    protected array $config;
 
-    /** @var array */
-    protected $config;
+    protected array $structure;
 
-    /** @var null */
-    protected $filename;
-
-    /** @var string */
-    private $token;
-
-    /** @var string */
-    private $authType;
-
-    /** @var array */
-    private const AUTH_OPTIONS = [
-        'bearer',
-        'basic',
-    ];
-
-    /** @var \Illuminate\Validation\Validator */
-    private $validator;
-
-    public function __construct(Router $router, Repository $config)
+    public function handle(Router $router, Repository $config): void
     {
-        parent::__construct();
-
         $this->router = $router;
         $this->config = $config['api-postman'];
-    }
 
-    public function handle(): void
-    {
         $filename = str_replace(
             ['{timestamp}', '{app}'],
             [date('Y_m_d_His'), Str::snake(config('app.name'))],
             $this->config['filename']
         );
 
-        $mapper = (new PostmanMapper)
+        $mapper = (new PostmanExporter)
             ->setFilename($filename);
 
         Storage::disk($this->config['disk'])
