dd27cc15abc732c5a62bbba77d437975b01ded86
tomirons <tom.irons@hotmail.com>
2023-09-05 09:15:18 -0400

    create auth methods

---
+++ src/Authentication/AuthenticationMethod.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AndreasElia\PostmanGenerator\Authentication;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+abstract class AuthenticationMethod implements Arrayable
+{
+    //
+}
