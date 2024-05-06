f41edfb7bfe4759984a677fef29a40e7da083730
Andreas Elia <andreaselia@live.co.uk>
2024-05-06 22:22:49 +0100

    update controller

---
+++ tests/Fixtures/ExampleModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AndreasElia\PostmanGenerator\Tests\Fixtures;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExampleModel extends Model
+{
+}
--- tests/Fixtures/PostController.php
+++ tests/Fixtures/PostController.php
@@ -19,7 +19,7 @@ class PostController extends Controller
     {
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, ExampleModel $post)
     {
     }
 
